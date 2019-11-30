6d77c988e4ceccb2255a7110f2d5392c81a3d548
Jaky <jkueper@uwo.ca>
2019-11-30 15:14:39 -0500

    Add files via upload

--- splitDuplicate.py
+++ splitDuplicate.py
@@ -4,8 +4,37 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import scipy.stats as stats
 
+
 #import data - change filename in future
-df = pd.read_csv('yrbs1517')
+df = pd.read_csv('dfManual')
+
+#note this script assumes names: 'modOneOutcome', 'modTwoOutcome'
+# one is illicit drugs; two is tobacco and alcohol 
+
+###################################
+# GET SET SIZES
+###################################
+#test set
+#bounded loss apporach
+# d = 0.015
+testSize = int(((2 * 0.5) / 0.015)**2) #4444
+
+#validation set
+#to check the probability of correct model selection, with different validation test sizes
+# loc is mean = -0.01 for 1% model difference 
+# scale is std deviation = depends on n; using 0.5 for bound on loss standard deviation in formula
+# x is the value to go up to = 0 for getting the negative quadrant
+#tried the below probability calculation for several SD values; n = 8500 gives about 90% confidence of selecting better of two models 
+
+#  stats.norm.cdf(x = 0, loc = -0.01, scale = 0.0076696)
+
+validationSize = 8500
+
+
+
+###################################
+#data splitting
+###################################
 
 
 #multiply weight column by 100
@@ -23,43 +52,13 @@ df['weightMultiplied'] = df['weight'] * 100
 # print("Shape of data", df.shape)
 # print("sum of all weights  adn other stuff", df['weight'].describe())
 
-###################################
-#data splitting
-###################################
-
-# GET SET SIZES
-
-# note these will be updated once we know what rows to remove 
-
-#test set
-#bounded loss apporach
-# d = 0.015
-testSize = int(((2 * 0.5) / 0.015)**2) #4444
-
-#validation set
-
-#to check the probability of correct model selection, with different validation test sizes
-# loc is mean = -0.01 for 1% model difference 
-# scale is std deviation = depends on n; using 0.5 for bound on loss standard deviation in formula
-# x is the value to go up to = 0 for getting the negative quadrant
-#tried the below probability calculation for several SD values; n = 8500 gives about 90% confidence of selecting better of two models 
-
-#  stats.norm.cdf(x = 0, loc = -0.01, scale = 0.0076696)
-
-validationSize = 8500
-
-#test will be: 17445
-
 
 # SEPARATE OUTCOMES FROM PREDICTORS
 
-#extract outcomes - names may differ 
-
+#extract outcomes - names may differ
 y = df[['weightMultiplied', 'modOneOutcome', 'modTwoOutcome']]
 
 #extract predictors
-
-#need to have df more cleaned before this step
 X = df.drop(['modOneOutcome', 'modTwoOutcome'], axis = 'columns')
 
 #get training set
@@ -76,9 +75,8 @@ Xvalidate, Xtest, yvalidate, ytest = train_test_split(XRemaining, yRemaining, te
 
 
 ###################################
-#Weighting stuff
+#Duplicate training by weights
 ###################################
-
 
 #to check duplication
 #print("sum of weights in  X training set\n", Xtrain['weightMultiplied'].sum())
@@ -90,15 +88,29 @@ Xvalidate, Xtest, yvalidate, ytest = train_test_split(XRemaining, yRemaining, te
 #multiply training set by weights
 
 XtrainDuplicate = Xtrain.loc[Xtrain.index.repeat(Xtrain.weightMultiplied)]
-
 #print("shape x duplidated", XtrainDuplicate.shape)
 
 ytrainDuplicate = ytrain.loc[ytrain.index.repeat(ytrain.weightMultiplied)]
-
 #print("shape y duplidated", ytrainDuplicate.shape)
 
 
+#export the dfs
+Xtrain.to_csv(r' ', index = False)
+ytrain.to_csv(r' ', index = False)
+XtrainDuplicate.to_csv(r' ', index = False)
+ytrainDuplicate.to_csv(r' ', index = False)
+Xvalidate.to_csv(r' ', index = False)
+yvalidate.to_csv(r' ', index = False)
+Xtest.to_csv(r' ', index = False)
+ytest.to_csv(r' ', index = False) 
+
+
+
+
+
+
 #next steps
+
 #train models on
 # ytrain - no duplicated
 # ytrainDuplicate - duplicated by factor of 100 * weight (may change)
